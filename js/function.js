2c248fa19efb6cf81adfbeb1cbe7ae624d21c190
Ankit Gupta <ankit9798739110@gmail.com>
2026-08-13 14:36:34 +0530

    function in js

---
+++ js/function.js
@@ -0,0 +1,13 @@
+
+
+      function abes( a,b){
+        return a+b;
+      }
+
+       let result=abes(10,20);
+      console.log(result);
+
+      // arrow function
+     const abes=()=>"hello";
+     console.log(abes());
+    
