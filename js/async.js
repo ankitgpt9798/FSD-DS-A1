00d8d5def19e2ce84b6ae0ee9777bbc04b2466f0
Ankit Gupta <ankit9798739110@gmail.com>
2026-08-21 09:36:10 +0530

    sync and async method in js

---
+++ js/async.js
@@ -0,0 +1,11 @@
+console.log("task1")
+console.log("task2")
+setTimeout (()=>{
+   console.log("task3")
+},5000)
+console.log("task4")
+console.log("task5")
+
+setTimeout(()=>{
+
+})
---
+++ js/sync.js
@@ -0,0 +1,6 @@
+console.log("task1");
+for(let i=1;i<=500000;i=i+100){
+    console.log(i)
+}
+console.log("task3")
+console.log("task4")
